cfe53dac8024e798b85487e3bbd0785b4c85b1d0
David Minker <davidminker@gmail.com>
2016-11-05 17:06:10 +0100

    checkDateValidity() implementiert.

--- Weihnachtskalender/Controller.cs
+++ Weihnachtskalender/Controller.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,22 +9,47 @@ namespace Weihnachtskalender
 {
     class Controller
     {
+        AddDataHandler addDataHandler = new AddDataHandler();
+
         private int _currentDate;
+
 
         //Konstruktor
         public Controller()
         {
             getSystemDate();
+            addDataHandler.readXML();
         }
 
         public void start(int btn)
         {
-
+            if (checkDateValidity(btn) == true)
+            {
+                //Do something
+            }
+            else
+            {
+                MessageBox.Show("You did select the wrong day.", "Oh oh...");
+            }
+            
         }
 
         private void getSystemDate()
         {
             _currentDate = Convert.ToInt16(DateTime.Now.Day);
+        }
+
+        private bool checkDateValidity(int btnTag)
+        {
+            if ( _currentDate >= btnTag )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
     }
